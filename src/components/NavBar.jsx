c7b28cf388eb2af70783d817d8bb835e8d7523ea
safchik <safchik9@gmail.com>
2023-10-09 16:08:00 +0100

    basic setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+
+const NavBar = () => {
+    return (
+        <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
