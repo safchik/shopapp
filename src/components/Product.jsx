66cbc8e1ced21f6a32f8fb9d3602dd4616d99758
safchik <safchik9@gmail.com>
2023-10-09 18:01:45 +0100

    made template with products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { BsPlus, BsEyeFill } from 'react-icons/bs'
 
-const Product = () => {
+const Product = ({ product }) => {
+    const { id, image, category, title, price } = product;
     return (
-        <div>Product</div>
+        <div>
+            <div className='border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition'>
+                <div className='w-full h-full flex justify-center items-center'>
+                    {/* image */}
+                    <div className='w-[200px] mx-auto flex justify-center items-center'>
+                        <img className='max-h-[160px] group-hover:scale-110 transition duration-300' src={image} alt="" />
+                    </div>
+                </div>
+                <div className='absolute top-6 -right-11 group-hover:right-5 p-2 flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-500'>
+                    <button>
+                        <div className='flex justify-center items-center text-white w-12 h-12 bg-red-500'>
+                            <BsPlus className='text-3xl' />
+                        </div>
+                    </button>
+                    <Link to={`/product/${id}`} className='w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl'><BsEyeFill /></Link>
+                </div>
+            </div>
+            <div>2</div>
+        </div>
     )
 }
 
