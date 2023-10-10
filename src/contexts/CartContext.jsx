3c460ab0c7d7dccda2ac252f76a908d4b1752fc9
safchik <safchik9@gmail.com>
2023-10-10 11:13:12 +0100

    solution for adding items to cart in process

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 
-const CartContext = () => {
-    return (
-        <div>CartContext</div>
-    )
-}
+export const CartContext = createContext();
 
-export default CartContext
+const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (product, id) => {
+        const newItem = { ...product, amount: 1 };
+        const cartItem = cart.find((item) => {
+            return item.id === id;
+        });
+        //if cart item is already in the cart
+        if (cartItem) {
+            const newCart = [...cart].map((item) => {
+                if (item.id === id) {
+                    return { ...item, amount: cartItem.amount + 1 };
+                } else {
+                    return item;
+                }
+            });
+            setCart(newCart);
+        } else {
+            setCart([...cart, newItem]);
+        }
+    };
+    return <CartContext.Provider value={{ addToCart }}>{children}</CartContext.Provider>;
+};
+
+export default CartProvider;
