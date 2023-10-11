6ab38bfbab19a96c15abaa9cdd133d9114ccd4b8
safchik <safchik9@gmail.com>
2023-10-11 20:41:25 +0100

    error with fetching fakeapi in productcontext

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -34,7 +34,7 @@ const Sidebar = () => {
                     </div>
                 </div>
                 <Link to='/' className='bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium'>View cart</Link>
-                <Link to='/' className='bg-primary flex p-4 justify-center items-center text-white w-full font-medium'>Checkout</Link>
+                <Link to='/' className='bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium'>Checkout</Link>
             </div>
         </div>
     );
